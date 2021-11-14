6088dc88aae312adf413f84d4afce4e457f11315
tejassonone <tejassonone01@gmail.com>
2021-11-14 13:46:45 +0530

    backend integration done

---
+++ travel/home/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home_page'),
+    path('signin/', views.sign, name='sign_in'),
+    path('explore/', views.explore, name='explore'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('profile/', views.profile, name='profile'),
+]
---
+++ travel/home/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def sign(request):
+    return render(request, 'signin_up.html')
+
+def explore(request):
+    return render(request, 'explore.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def profile(request):
+    return render(request, 'profile.html')
